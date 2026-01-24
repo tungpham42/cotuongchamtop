344c453efe709553b9382b6eb0484e4e8c573623
Tung Pham <tung.42@gmail.com>
2026-01-25 06:59:28 +0700

    Update analyzeBtn.blade.php

--- resources/views/layout/partials/analyzeBtn.blade.php
+++ resources/views/layout/partials/analyzeBtn.blade.php
@@ -1,5 +1,5 @@
 <div class="position-relative w-100 text-center mt-3 mt-md-2">
-    <button id="btn-analyze" class="btn btn-dark text-light mx-auto rounded-pill" title="Phân tích ván đấu bằng AI" style="min-width: 50px;">
-        <i class="fas fa-magic"></i> <span class="d-none d-md-inline">Phân tích</span>
+    <button id="btn-analyze" class="btn btn-lg btn-dark text-light mx-auto rounded-pill" title="Phân tích ván đấu bằng AI" style="min-width: 50px;">
+        <i class="fad fa-magic"></i> <span class="d-none d-md-inline">Phân tích</span>
     </button>
 </div>
