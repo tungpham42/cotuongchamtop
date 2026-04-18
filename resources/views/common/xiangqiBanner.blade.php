89fb10585e9ba1f5ecd70fe985125a55d2d9b49f
Tung Pham <tung.42@gmail.com>
2026-04-18 09:46:43 +0700

    Update xiangqiBanner.blade.php

--- resources/views/common/xiangqiBanner.blade.php
+++ resources/views/common/xiangqiBanner.blade.php
@@ -1,5 +1,5 @@
 <div style="background-color: #302E2B" class="container-fluid adsense px-0">
   <div class="row w-100 mx-auto">
-    <a class="w-100 hoc-link" href="https://www.facebook.com/groups/HoiChoiCoTuong"><img title="Cờ tướng" alt="Cờ tướng" class="w-100 h-auto mx-auto d-block" src="{{ $cdnUrl }}/img/xiangqi-board.jpg" ></a>
+    <a class="w-100 hoc-link" href="https://blog.cotuong.top"><img title="Cờ tướng" alt="Cờ tướng" class="w-100 h-auto mx-auto d-block" src="{{ $cdnUrl }}/img/xiangqi-board.jpg" ></a>
   </div>
 </div>
