5a5d4b7c444478388b9593ff729f1d3a7a9a69b3
Tung Pham <tung.42@gmail.com>
2026-05-18 08:18:48 +0700

    Update head.blade.php

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -7,9 +7,9 @@
 <meta name="theme-color" content="#f04124" >
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta property="og:image" content="@yield('og_image', url('/') . '/img/1200x630.jpg')">
-<meta property="og:image:width" content="1200" >
-<meta property="og:image:height" content="630" >
-<meta property="og:image:alt" content="Cờ tướng 2 người" >
+<meta property="og:image:width" content="@yield('og_image_width', '1200')" >
+<meta property="og:image:height" content="@yield('og_image_height', '630')" >
+<meta property="og:image:alt" content="@yield('og_image_alt', 'Cờ tướng 2 người')" >
 <link rel="apple-touch-icon" href="{{ url('/') }}/img/app-icons/apple-touch-icon-iphone-game.png">
 <link rel="apple-touch-icon" sizes="76x76" href="{{ url('/') }}/img/app-icons/apple-touch-icon-ipad-game.png">
 <link rel="apple-touch-icon" sizes="120x120" href="{{ url('/') }}/img/app-icons/apple-touch-icon-iphone-retina-game.png">
@@ -22,11 +22,9 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icons/7.1.0/css/flag-icons.min.css" integrity="sha512-bZBu2H0+FGFz/stDN/L0k8J0G8qVsAL0ht1qg5kTwtAheiXwiRKyCq1frwfbSFSJN3jooR5kauE0YjtPzhZtJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.9.3/introjs.min.css" integrity="sha512-DcHJLWkmfnv+isBrT8M3PhKEhsHWhEgulhr8m5EuGhdAG9w+vUyjlwgR4ISLN0+s/m4ItmPsTOqPzW714dtr5w==" crossorigin="anonymous" referrerpolicy="no-referrer">
 {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pace/1.2.4/themes/red/pace-theme-loading-bar.min.css" integrity="sha512-L7L86P7/Kgjmnc8/oz6wEpwzWXf2ezUPLgOt+bAdquy7SA0kK8ZtAFrTQpJN0mdtkILJ71UsRTL8sZGRS9bs1g==" crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
-<!-- Google Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&family=Pacifico&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Roboto:ital,wght@0,100..900;1,100..900&family=Texturina:ital,opsz,wght@0,12..72,100..900;1,12..72,100..900&display=swap" rel="stylesheet">
-<!-- End Google Fonts -->
 <link href="{{ url('/') }}/css/index.css?v=321" rel="stylesheet">
 @if (!($showAds ?? true))
 <style>
@@ -43,7 +41,6 @@
 @if ($showAds ?? true)
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3585118770961536" crossorigin="anonymous"></script>
 @endif
-<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-QEW6K9YPY7"></script>
 <script>
 window.dataLayer = window.dataLayer || [];
@@ -52,13 +49,11 @@ gtag('js', new Date());
 
 gtag('config', 'G-QEW6K9YPY7');
 </script>
-<!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WM9GZXN');</script>
-<!-- End Google Tag Manager -->
 @include('common.schemaOrg')
 @if ($showAds ?? true)
 <script type="text/javascript" src="{{ url('/') }}/js/aclib.js"></script>
@@ -66,7 +61,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <script type="text/javascript" src="{{ url('/') }}/js/theme-manager.js?v=1"></script>
 
 @guest
-<!--- Google One Tap Sign-in -->
 <script src="https://accounts.google.com/gsi/client" async defer></script>
 
 <div id="g_id_onload"
