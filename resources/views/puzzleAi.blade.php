792b9ec655ac3a4033a957e6ae82053b147abb6f
Tung Pham <tung.42@gmail.com>
2026-05-22 10:51:53 +0700

    Update puzzleAi.blade.php

--- resources/views/puzzleAi.blade.php
+++ resources/views/puzzleAi.blade.php
@@ -5,7 +5,7 @@
 
 {{-- Set the dynamic Open Graph Image and Alt text using a placeholder service --}}
 @if($puzzleName)
-    @section('og_image', 'https://placehold.co/1080x1080/DFBD85/725834/jpeg?font=roboto&text=' . urlencode($puzzleName))
+    @section('og_image', 'https://placehold.co/1080x1080/DFBD85/725834/jpeg?font=roboto&text=' . urlencode(__("Thế cờ")) . "\n" . urlencode($puzzleName))
     @section('og_image_alt', $puzzleName)
     @section('og_image_width', '1080')
     @section('og_image_height', '1080')
