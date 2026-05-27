8acd24229507e78a18a573d8d6496cd5a26d6ea6
Tung Pham <tung.42@gmail.com>
2026-05-28 06:59:14 +0700

    border-dark

--- resources/views/tournaments/create.blade.php
+++ resources/views/tournaments/create.blade.php
@@ -13,7 +13,7 @@
                         @csrf
                         <div class="form-group text-dark">
                             <label>{{ __('Tên Giải Đấu') }} <span class="text-danger">*</span></label>
-                            <input type="text" name="name" class="form-control bg-dark text-dark border-secondary" required>
+                            <input type="text" name="name" class="form-control bg-light text-dark border-dark" required>
                         </div>
 
                         <div class="form-group text-dark">
@@ -23,23 +23,23 @@
 
                         <div class="form-group text-dark">
                             <label>{{ __('Mô tả') }}</label>
-                            <textarea name="description" class="form-control bg-light text-dark border-secondary" rows="3"></textarea>
+                            <textarea name="description" class="form-control bg-light text-dark border-dark" rows="3"></textarea>
                         </div>
 
                         <div class="row">
                             <div class="col-md-4 form-group text-dark">
                                 <label>{{ __('Ngày Bắt Đầu') }} <span class="text-danger">*</span></label>
-                                <input type="datetime-local" name="start_date" class="form-control bg-light text-dark border-secondary" required>
+                                <input type="datetime-local" name="start_date" class="form-control bg-light text-dark border-dark" required>
                             </div>
 
                             <div class="col-md-4 form-group text-dark">
                                 <label>{{ __('Số lượng kỳ thủ') }} <span class="text-danger">*</span></label>
-                                <input type="number" name="max_players" class="form-control bg-light text-dark border-secondary" min="2" max="16" value="16" required>
+                                <input type="number" name="max_players" class="form-control bg-light text-dark border-dark" min="2" max="16" value="16" required>
                             </div>
 
                             <div class="col-md-4 form-group text-dark">
                                 <label>{{ __('Trạng thái') }} <span class="text-danger">*</span></label>
-                                <select name="status" class="form-control bg-light text-dark border-secondary" required>
+                                <select name="status" class="form-control bg-light text-dark border-dark" required>
                                     <option value="open">{{ __('Mở đăng ký') }}</option>
                                     <option value="in_progress">{{ __('Đang diễn ra') }}</option>
                                     <option value="completed">{{ __('Đã kết thúc') }}</option>
