92a2e6f764cdb988126ca12f6f7a76010014ebe9
Tung Pham <tung.42@gmail.com>
2026-06-02 17:29:14 +0700

    cờ thế title

--- resources/views/layout/partials/app/head.blade.php
+++ resources/views/layout/partials/app/head.blade.php
@@ -23,8 +23,8 @@
     }
 @endphp
 
-<meta property="og:title" content="{{ __(':title - Cờ tướng 2 người, đánh cờ tướng online, chơi cờ tướng với máy miễn phí', ['title' => $siteTitle]) }}" >
-<title>{{ __(':title - Cờ tướng 2 người, đánh cờ tướng online, chơi cờ tướng với máy miễn phí', ['title' => $siteTitle]) }}</title>
+<meta property="og:title" content="{{ __(':title - Cờ tướng 2 người, Cờ thế, đánh cờ tướng online, chơi cờ tướng với máy miễn phí', ['title' => $siteTitle]) }}" >
+<title>{{ __(':title - Cờ tướng 2 người, Cờ thế, đánh cờ tướng online, chơi cờ tướng với máy miễn phí', ['title' => $siteTitle]) }}</title>
 @include('common.head')
 @include('common.scripts')
 <script>
--- resources/views/layout/partials/head.blade.php
+++ resources/views/layout/partials/head.blade.php
@@ -1,9 +1,9 @@
 <meta charset="utf-8" >
-<title>{{ url()->current() == url('') ? __('Cờ tướng 2 người, đánh cờ tướng online, chơi cờ tướng với máy miễn phí') : __(':title - Cờ tướng 2 người, đánh cờ tướng online, chơi cờ tướng với máy miễn phí', ['title' => $headTitle]) }}</title>
+<title>{{ url()->current() == url('') ? __('Cờ tướng 2 người, Cờ thế, đánh cờ tướng online, chơi cờ tướng với máy miễn phí') : __(':title - Cờ tướng 2 người, Cờ thế, đánh cờ tướng online, chơi cờ tướng với máy miễn phí', ['title' => $headTitle]) }}</title>
 <meta name="keywords" content="{{ __('cờ tướng 2 người, chơi cờ tướng với máy, choi co tuong voi may, cờ tướng hay, co tuong, game co tuong online, co tuong 2 nguoi, cờ tướng, chơi cờ tướng online, choi co tuong online, choi co tuong, chơi cờ tướng, cotuong, xiangqi, xiang qi') }}" >
 <meta property="article:tag" content="{{ __('cờ tướng') }}">
 <meta name="description" content="{{ __('Cùng chơi với nhiều tính năng hấp dẫn như cờ tướng 2 người, cờ tướng online, chơi cờ tướng với máy, cờ thế và Thi đấu xếp hạng!') }}" >
-<meta property="og:title" content="{{ url()->current() == url('') ? __('Cờ tướng 2 người, cờ tướng online, chơi cờ tướng với máy') : __(':title - Cờ tướng 2 người, đánh cờ tướng online, chơi cờ tướng với máy miễn phí', ['title' => $headTitle]) }}" >
+<meta property="og:title" content="{{ url()->current() == url('') ? __('Cờ tướng 2 người, cờ tướng online, chơi cờ tướng với máy') : __(':title - Cờ tướng 2 người, Cờ thế, đánh cờ tướng online, chơi cờ tướng với máy miễn phí', ['title' => $headTitle]) }}" >
 <meta property="og:description" content="{{ __('Cùng chơi với nhiều tính năng hấp dẫn như cờ tướng 2 người, cờ tướng online, chơi cờ tướng với máy, cờ thế và Thi đấu xếp hạng!') }}" >
 @include('common.head')
 @include('common.url')
