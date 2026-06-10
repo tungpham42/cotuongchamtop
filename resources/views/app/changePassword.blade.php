f65610179f372e3268e6997b39532d8260ffbff1
Tung Pham <tung.42@gmail.com>
2026-06-10 11:16:57 +0700

    adsenseBelowHeader

--- resources/views/app/changePassword.blade.php
+++ resources/views/app/changePassword.blade.php
@@ -2,21 +2,6 @@
 
 @section('content')
 <div class="container">
-    @if ($showAds ?? true)
-    <div class="row justify-content-center text-center mb-4">
-        <div class="col-12">
-            <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-3585118770961536"
-            data-ad-slot="7831723879"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-            <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-        </div>
-    </div>
-    @endif
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
