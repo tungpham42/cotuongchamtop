280ecffb7b73c98977495c127bf1b48e2e52a9ae
Tung Pham <tung.42@gmail.com>
2026-06-10 12:10:43 +0700

    Update adsenseTop.blade.php

--- resources/views/common/adsenseTop.blade.php
+++ resources/views/common/adsenseTop.blade.php
@@ -1,15 +1,14 @@
 @if ($showAds ?? true)
 {{-- AdSense Top Banner --}}
-<div class="container mt-3 text-center">
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3585118770961536" crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-3585118770961536"
-        data-ad-slot="7831723879"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-<script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<div class="container mt-3 text-center" style="min-height: 100px;">
+    <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-3585118770961536"
+            data-ad-slot="7831723879"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+    <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </div>
 @endif
