52ba9e0e7f0305a79937078cf4d2dedf8746ffde
Tung Pham <tung.42@gmail.com>
2026-06-10 12:39:45 +0700

    multiplex ads

--- resources/views/common/adsenseBetween.blade.php
+++ resources/views/common/adsenseBetween.blade.php
@@ -2,13 +2,12 @@
 <div class="row justify-content-center text-center my-5">
     <div class="col-12">
         <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-3585118770961536"
-        data-ad-slot="4773707309"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
+            style="display:block"
+            data-ad-format="autorelaxed"
+            data-ad-client="ca-pub-3585118770961536"
+            data-ad-slot="7285196287"></ins>
         <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
+            (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
     </div>
 </div>
--- resources/views/common/adsenseTop.blade.php
+++ resources/views/common/adsenseTop.blade.php
@@ -2,13 +2,12 @@
 {{-- AdSense Top Banner --}}
 <div class="container mt-3 text-center" style="min-height: 100px;">
     <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-3585118770961536"
-            data-ad-slot="7831723879"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
+        style="display:block"
+        data-ad-format="autorelaxed"
+        data-ad-client="ca-pub-3585118770961536"
+        data-ad-slot="8034295460"></ins>
     <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
+        (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
 </div>
 @endif
