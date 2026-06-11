c201ed27ebf442b6f5be51ab5060db9fb532de2a
Tung Pham <tung.42@gmail.com>
2026-06-12 05:54:16 +0700

    Update puzzle.blade.php

--- resources/views/puzzle.blade.php
+++ resources/views/puzzle.blade.php
@@ -52,7 +52,7 @@
   <div class="input-group-prepend">
     <span class="input-group-text" id="url-addon"><i class="fal fa-copy"></i></span>
   </div>
-  <input data-step="6" data-intro="Ấn vào đây để mời bạn bè cùng chơi" type="text" class="form-control" id="url" value="{{ url('/') }}{{ __('/co-the/')' }}{{ $board }}">
+  <input data-step="6" data-intro="Ấn vào đây để mời bạn bè cùng chơi" type="text" class="form-control" id="url" value="{{ url('/') }}{{ __('/co-the/') }}{{ $board }}">
 </div>
 <script>
 $('#copy-url').on('click', function() {
