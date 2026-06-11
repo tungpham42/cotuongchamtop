e7654b80354200f761d683af404d91140efb8469
Tung Pham <tung.42@gmail.com>
2026-06-12 06:16:57 +0700

    Update adsenseTop.blade.php

--- resources/views/common/adsenseTop.blade.php
+++ resources/views/common/adsenseTop.blade.php
@@ -1,10 +1,10 @@
 @if ($showAds ?? true)
 {{-- AdSense Top Banner --}}
-<div class="container mt-3 text-center" style="min-height: 100px;">
+<div class="container my-0 mx-auto p-0 text-center" style="min-height: 100px;">
     <ins class="adsbygoogle"
             style="display:block"
             data-ad-client="ca-pub-3585118770961536"
-            data-ad-slot="7831723879"
+            data-ad-slot="9745772897"
             data-ad-format="auto"
             data-full-width-responsive="true"></ins>
     <script>
