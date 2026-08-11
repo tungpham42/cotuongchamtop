a70bc888d90d9201e649f64bfa8096abbe90ed3c
Tung Pham <tung.42@gmail.com>
2026-08-11 16:44:27 +0700

    remove aclib

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -79,9 +79,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WM9GZXN');</script>
 @include('common.schemaOrg')
-@if ($showAds ?? true)
-<script type="text/javascript" src="{{ asset('js/aclib.js') }}"></script>
-@endif
 <script type="text/javascript" src="{{ asset('js/theme-manager.js?v=1') }}"></script>
 
 @guest
