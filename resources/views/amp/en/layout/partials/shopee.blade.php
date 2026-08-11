57923eaff20690ed28e55914a16700b9f54d5172
Tung Pham <tung.42@gmail.com>
2026-08-11 07:29:42 +0700

    remove amp

--- resources/views/amp/en/layout/partials/shopee.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container-fluid adsense px-0 border-top">
-  <div class="container mx-auto px-0">
-    <div class="row w-100 mx-auto py-5 px-0">
-      <div class="mb-4 col-12 px-0">
-        <img class="w-100" src="{{ url('/') }}/img/me_va_be.png" />
-      </div>
-    </div>
-  </div>
-</div>
