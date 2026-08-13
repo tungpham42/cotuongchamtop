6f8ab3ffc45a4a6643cb7ff998f2de492962400d
Tung Pham <tung.42@gmail.com>
2026-08-13 17:31:07 +0700

    Update AdminUserController.php

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -42,7 +42,7 @@ class AdminUserController extends Controller
             'name'              => 'required|string|max:255',
             'email'             => 'required|string|email|max:255|unique:users',
             'password'          => 'required|string|min:8',
-            'elo'               => 'nullable|float',
+            'elo'               => 'nullable',
             'is_admin'          => 'boolean',
             'subscription_plan' => 'nullable|string',
         ]);
@@ -65,7 +65,7 @@ class AdminUserController extends Controller
         $validated = $request->validate([
             'name'              => 'required|string|max:255',
             'email'             => "required|string|email|max:255|unique:users,email,{$user->id}",
-            'elo'               => 'required|float',
+            'elo'               => 'required',
             'subscription_plan' => 'nullable|string',
         ]);
 
