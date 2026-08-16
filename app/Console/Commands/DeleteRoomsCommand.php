1181d4f110d810ddda0dbd57294bcd78149e60a1
Tung Pham <tung.42@gmail.com>
2026-08-16 16:47:46 +0700

    Update DeleteRoomsCommand.php

--- app/Console/Commands/DeleteRoomsCommand.php
+++ app/Console/Commands/DeleteRoomsCommand.php
@@ -39,27 +39,32 @@ class DeleteRoomsCommand extends Command
                 $query->where('fen', '=', 'rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r - - 0 1')
                       ->orWhere('fen', '=', 'rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r - - 0 1 resign');
             })
+            ->whereNull('tournament_id')
             ->delete();
 
         Room::where('modified_at', '<', $date)
             ->whereNull('host_id')
             ->whereNull('result')
+            ->whereNull('tournament_id')
             ->delete();
 
         Room::whereNotNull('host_id')
             ->whereNotNull('result')
             ->whereNull('guest_id')
+            ->whereNull('tournament_id')
             ->delete();
 
         Room::where('modified_at', '<', $date)
             ->whereNotNull('host_id')
             ->whereNotNull('result')
+            ->whereNull('tournament_id')
             ->where('fen', 'LIKE', '% - - 0 1%')
             ->delete();
 
         // Delete rooms where host_id or guest_id doesn't exist in users table
         Room::whereNotIn('host_id', User::pluck('id')->toArray())
             ->orWhereNotIn('guest_id', User::pluck('id')->toArray())
+            ->whereNull('tournament_id')
             ->delete();
 
         $this->info('Rooms older than ' . $days . ' days and rooms with missing users have been deleted.');
