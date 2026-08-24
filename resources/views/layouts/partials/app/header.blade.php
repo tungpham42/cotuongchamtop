cb36f6ffd8fee073196f0e1230ca77fcc5128ec0
Tung Pham <tung.42@gmail.com>
2026-08-24 18:11:28 +0700

    Bài viết

--- resources/views/layouts/partials/app/header.blade.php
+++ resources/views/layouts/partials/app/header.blade.php
@@ -65,7 +65,7 @@
           </li>
 
           <li class="nav-item">
-            <a class="showPromotion" href="{{ localized_url('article.index') }}"><i class="far fa-newspaper"></i> {{ __("Tin tức") }}</a>
+            <a class="showPromotion" href="{{ localized_url('article.index') }}"><i class="far fa-newspaper"></i> {{ __("Bài viết") }}</a>
           </li>
           <li class="nav-item">
             <a class="showPromotion" target="_blank" href="https://blog.cotuong.top"><i class="far fa-blog"></i> {{ __("Blog") }}</a>
