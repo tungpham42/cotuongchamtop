e809cb1076d74cc8e2177358c458c71e68b59ffa
Tung Pham <tung.42@gmail.com>
2026-08-29 16:10:11 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,7 +40,7 @@ class LoginController extends Controller
      */
     private function storePreviousUrl()
     {
-        if (!str_contains(url()->previous(), localized_url('login')) || !str_contains(url()->previous(), localized_url('register'))) {
+        if (!str_contains(url()->previous(), localized_url('login')) && !str_contains(url()->previous(), localized_url('register'))) {
             Session::put('previousUrl', url()->previous());
         }
     }
