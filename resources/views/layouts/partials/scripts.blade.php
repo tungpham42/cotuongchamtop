5ed9b3940a8081eda8e31f347173e0881e2c4cf2
Tung Pham <tung.42@gmail.com>
2026-08-29 09:41:45 +0700

    css

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -293,7 +293,7 @@
         if ('serviceWorker' in navigator) {
             console.log("Will the service worker register?");
             navigator.serviceWorker
-                .register("{{ asset('serviceWorker.js?v=7') }}")
+                .register("{{ asset('serviceWorker.js?v=8') }}")
                 .then(function(reg) {
                     console.log("Yes, it did.");
                 }).catch(function(err) {
