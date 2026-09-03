764c079adfbf64e00ddb48fb216fa8e1cb5273cf
Tung Pham <tung.42@gmail.com>
2026-09-03 15:08:03 +0700

    Update show.blade.php

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -390,7 +390,8 @@
         </div>
     </div>
 </div>
-
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     $(document).ready(function() {
         const dbInitialFen = @json($game->initial_fen);
