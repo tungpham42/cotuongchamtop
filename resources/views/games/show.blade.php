8cf1f2c1fda5554b04268366dddce1bc554d284f
Tung Pham <tung.42@gmail.com>
2026-09-03 16:16:17 +0700

    Update show.blade.php

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title', $game->title)
-@section('og_image', 'https://placehold.co/1200x630/DA251D/FFFF00/jpeg?font=roboto&text=' . urlencode($game->description))
+@section('og_image', 'https://placehold.co/1200x630/DA251D/FFFF00/jpeg?font=roboto&text=' . urlencode($game->title))
 @section('meta_description', Str::limit($game->description, 150))
 
 @section('content')
