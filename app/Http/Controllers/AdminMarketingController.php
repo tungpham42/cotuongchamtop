ea9ad6e4e4d8aff3a9db8ccfe2b8ae96ba127f3e
Tung Pham <tung.42@gmail.com>
2026-09-15 11:23:33 +0700

    Update AdminMarketingController.php

--- app/Http/Controllers/AdminMarketingController.php
+++ app/Http/Controllers/AdminMarketingController.php
@@ -8,6 +8,7 @@ use App\Models\KarmaLog;
 use App\Models\Room;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminMarketingController extends Controller
 {
