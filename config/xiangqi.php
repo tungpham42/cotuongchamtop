b6858cb26272af74d39ff3737f177d913bcf09c3
Tung Pham <tung.42@gmail.com>
2026-09-21 12:55:52 +0700

    fix pool

--- config/xiangqi.php
+++ config/xiangqi.php
@@ -10,4 +10,6 @@ return [
 
     // Must match the numprocs value in the supervisor config.
     'worker_count' => env('XIANGQI_WORKER_COUNT', 5),
+
+    'php_binary' => env('XIANGQI_PHP_BINARY'),
 ];
