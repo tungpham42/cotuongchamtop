fe4468454d051d448c3de26cc4d2a8a5bd9601c4
Tung Pham <tung.42@gmail.com>
2026-09-21 10:22:06 +0700

    login to show hint

--- resources/views/layouts/partials/hint.blade.php
+++ resources/views/layouts/partials/hint.blade.php
@@ -10,3 +10,18 @@
     </a>
 </div>
 @endauth
+
+@guest
+<div class="hint-widget mx-auto text-center my-1" id="hint-widget">
+    <a id="hint-btn-guest"
+        href="javascript:void(0)"
+        class="btn btn-dark btn-lg"
+        onclick="var n = document.getElementById('hint-login-notice'); n.classList.remove('d-none'); clearTimeout(window._hintNoticeTimer); window._hintNoticeTimer = setTimeout(function () { n.classList.add('d-none'); }, 3000);"
+        >
+        <i class="fad fa-lightbulb-on"></i> {{ __('Gợi ý') }}
+    </a>
+    <div id="hint-login-notice" class="alert alert-dark d-none mt-2 mb-0" role="alert">
+        <i class="fad fa-lock"></i> {{ __('Đăng nhập để xem gợi ý') }}
+    </div>
+</div>
+@endguest
