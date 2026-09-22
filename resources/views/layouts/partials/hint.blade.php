8663707b6029afc88f7b0c17101b69e54f78ea9c
Tung Pham <tung.42@gmail.com>
2026-09-22 10:43:11 +0700

    Update hint.blade.php

--- resources/views/layouts/partials/hint.blade.php
+++ resources/views/layouts/partials/hint.blade.php
@@ -15,12 +15,12 @@
 <div class="hint-widget mx-auto text-center my-1" id="hint-widget">
     <a id="hint-btn-guest"
         href="javascript:void(0)"
-        class="btn btn-dark btn-lg"
+        class="btn btn-dark btn-lg disabled"
         >
         <i class="fad fa-lightbulb-on"></i> {{ __('Gợi ý') }}
     </a>
     <div id="hint-login-notice" class="alert alert-dark mt-2 mb-0" role="alert">
-        <a href="{{ localized_url('login') }}"><i class="fad fa-lock"></i> {{ __('Đăng nhập để xem gợi ý') }}</a>
+        <a class="text-light" href="{{ localized_url('login') }}"><i class="fad fa-lock"></i> {{ __('Đăng nhập để xem gợi ý') }}</a>
     </div>
 </div>
 @endguest
